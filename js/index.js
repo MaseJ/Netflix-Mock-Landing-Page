be6f8407e59c76770f3aa55413b528dab6181fd4
MaseJ <mjohnson434@student.cvtc.edu>
2022-08-28 12:43:43 -0500

    Create basic layout for page

---
+++ js/index.js
@@ -0,0 +1,214 @@
+// Float animation for title and popcorn
+const floatAnimation = () => {
+    const tlLogo = new TimelineMax({repeat:-1});
+    
+   tlLogo
+       
+   .to('.floating', 3, { y:'-=30', x:'+=20',  rotation:'-=5', ease:Power1.easeInOut})
+   
+       
+   .to('.floating', 2, { y:'+=30', x:'-=20', rotation:'-=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating', 3, { y:'-=20',  rotation:'+=5', ease:Power1.easeInOut})
+   
+   .to('.floating', 3, { y:'+=20',  rotation:'+=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating', 3, { y:'-=50', ease:Power1.easeInOut})
+      
+   .to('.floating', 3, { y:'+=50', ease:Power1.easeInOut})
+   
+   
+   .to('.floating', 3, { y:'-=30', ease:Power1.easeInOut})
+      
+   .to('.floating', 3, { y:'+=30', ease:Power1.easeInOut})
+   
+   
+   .to('.floating', 2, { y:'-=30', ease:Power1.easeInOut})
+      
+   .to('.floating', 2, { y:'+=30', ease:Power1.easeInOut})
+
+ TweenLite.to(tlLogo, 27, {ease:Power1.easeInOut})
+
+}
+floatAnimation();
+
+const floatAnimation2 = () => {
+    const tlLogo = new TimelineMax({repeat:-1});
+    
+   tlLogo
+       
+   .to('.floating2', 3, { y:'+=30', x:'+=20',  rotation:'-=15', ease:Power1.easeInOut})
+   
+       
+   .to('.floating2', 2, { y:'+=30', x:'-=20', rotation:'-=35', ease:Power1.easeInOut})
+   
+   
+   .to('.floating2', 3, { y:'-=20',  rotation:'+=15', ease:Power1.easeInOut})
+   
+   .to('.floating2', 3, { y:'-=20',  rotation:'+=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating2', 3, { y:'-=50', ease:Power1.easeInOut})
+      
+   .to('.floating2', 3, { y:'-=50', ease:Power1.easeInOut})
+   
+   
+   .to('.floating2', 3, { y:'-=10', ease:Power1.easeInOut})
+      
+   .to('.floating2', 3, { y:'+=40', ease:Power1.easeInOut})
+   
+   
+   .to('.floating2', 2, { y:'-=30', ease:Power1.easeInOut})
+      
+   .to('.floating2', 2, { y:'+=20', ease:Power1.easeInOut})
+
+ TweenLite.to(tlLogo, 27, {ease:Power1.easeInOut})
+
+}
+floatAnimation2();
+
+
+const floatAnimation3 = () => {
+    const tlLogo = new TimelineMax({repeat:-1});
+    
+   tlLogo
+       
+   .to('.floating3', 3, { y:'+=10', x:'-=20',  rotation:'-=15', ease:Power1.easeInOut})
+   
+       
+   .to('.floating3', 2, { y:'+=10', x:'+=20', rotation:'+=35', ease:Power1.easeInOut})
+   
+   
+   .to('.floating3', 3, { y:'-=5',  rotation:'+=35', ease:Power1.easeInOut})
+   
+   .to('.floating3', 3, { y:'-=20',  rotation:'-=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating3', 3, { y:'+=50', ease:Power1.easeInOut})
+      
+   .to('.floating3', 3, { y:'-=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating3', 3, { y:'+=10', ease:Power1.easeInOut})
+      
+   .to('.floating3', 3, { y:'+=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating3', 2, { y:'-=10', ease:Power1.easeInOut})
+      
+   .to('.floating3', 2, { y:'+=20', ease:Power1.easeInOut})
+
+ TweenLite.to(tlLogo, 27, {ease:Power1.easeInOut})
+
+}
+floatAnimation3();
+
+const floatAnimation4 = () => {
+    const tlLogo = new TimelineMax({repeat:-1});
+    
+   tlLogo
+       
+   .to('.floating4', 3, { y:'+=40', x:'-=20',  rotation:'+=15', ease:Power1.easeInOut})
+   
+       
+   .to('.floating4', 2, { y:'+=10', x:'-=20', rotation:'+=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating4', 3, { y:'+=5',  rotation:'+=5', ease:Power1.easeInOut})
+   
+   .to('.floating4', 3, { y:'-=40',  rotation:'-=5', ease:Power1.easeInOut})
+   
+   
+   .to('.floating4', 3, { y:'+=20', ease:Power1.easeInOut})
+      
+   .to('.floating4', 3, { y:'+=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating4', 3, { y:'+=40', ease:Power1.easeInOut})
+      
+   .to('.floating4', 3, { y:'-=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating4', 2, { y:'-=10', ease:Power1.easeInOut})
+      
+   .to('.floating4', 2, { y:'-=20', ease:Power1.easeInOut})
+
+ TweenLite.to(tlLogo, 27, {ease:Power1.easeInOut})
+
+}
+floatAnimation4();
+
+const floatAnimation5 = () => {
+    const tlLogo = new TimelineMax({repeat:-1});
+    
+   tlLogo
+       
+   .to('.floating5', 3, { y:'+=10', x:'-=20',  rotation:'+=80', ease:Power1.easeInOut})
+   
+       
+   .to('.floating5', 2, { y:'-=10', x:'-=20', rotation:'+=50', ease:Power1.easeInOut})
+   
+   
+   .to('.floating5', 3, { y:'-=5',  rotation:'-=20', ease:Power1.easeInOut})
+   
+   .to('.floating5', 3, { y:'+=40',  rotation:'-=50', ease:Power1.easeInOut})
+   
+   
+   .to('.floating5', 3, { y:'+=20', ease:Power1.easeInOut})
+      
+   .to('.floating5', 3, { y:'-=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating5', 3, { y:'+=40', ease:Power1.easeInOut})
+      
+   .to('.floating5', 3, { y:'+=20', ease:Power1.easeInOut})
+   
+   
+   .to('.floating5', 2, { y:'-=10', ease:Power1.easeInOut})
+      
+   .to('.floating5', 2, { y:'-=20', ease:Power1.easeInOut})
+
+ TweenLite.to(tlLogo, 27, {ease:Power1.easeInOut})
+
+}
+floatAnimation5();
+
+
+
+
+
+
+
+
+
+
+
+
+
+let tlMainBg = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 2}});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#main", {
+    scrollTrigger: {
+        trigger: "#second",
+        start: 'top bottom',
+        toggleActions: "play reset reverse reset",
+        scrub: 1
+    },
+    clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"
+});
+
+ let tlSecondPage = gsap.timeline({
+   scrollTrigger: {
+     trigger: "#scrollTrigger",
+     start: "bottom bottom",
+     toggleActions: "play none reverse reset",
+   }
+ })
+   .from("#movieText", {opacity: 0, duration: 2})
+   .to("#movieContainer", {opacity: .5, duration: 1.5}, "-=1");
+
+
